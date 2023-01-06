5251845ecc18b16737292fde02dbc080c7ab6d46
pcazin <pablocazin.dev@gmail.com>
2023-01-06 14:16:37 +0100

    mise en place de l'authentificaton et du router

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./index.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/homePage";
+import Login from "./views/loginPage";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/ProtectedPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <AuthProvider>
+          <Navbar />
+          <Switch>
+            <PrivateRoute component={ProtectedPage} path="/protected" exact />
+            <Route component={Students} path="/students" />
+            <Route component={StudentsDetails} path="/students/:id" />
+            <Route component={Fight} path="/fight" />
+            <Route component={Ranks} path="/ranks" />
+            <Route component={Historical} path="/historical" />
+            <Route component={Login} path="/login" />
+            <Route component={Register} path="/register" />
+            <Route component={Home} path="/" />
+          </Switch>
+        </AuthProvider>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
