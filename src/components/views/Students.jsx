88c4684ebc125f83785139b615a51326a2715560
TheoROB <theorobillard@live.com>
2023-01-06 17:03:40 +0100

    commencement de la page students

--- src/components/views/Students.jsx
+++ src/components/views/Students.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import '../../style/students.css';
 
 function Students() {
   return (
-    <p>Students Works !</p>
+    <>
+    <div className="menu">
+      <h1>Étudiants :</h1>
+        <div className="orderStudents">
+          <p>Age</p>
+          <p>Sexe</p>
+          <p>A-Z</p>
+        </div>
+        <div>
+          <img src="" alt="croix ajout etudiant" />
+          <img src="" alt="revenir en arriere" />
+        </div>
+    </div>
+    </>
+
+
   );
 }
 
